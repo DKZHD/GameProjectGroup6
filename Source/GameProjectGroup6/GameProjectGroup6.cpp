206909af8822cc92ca53c77bc0ef700d115ce7c8
DKZHD <dkvarberg@gmail.com>
2023-03-23 10:22:50 +0100

    Created Project

---
+++ Source/GameProjectGroup6/GameProjectGroup6.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GameProjectGroup6.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GameProjectGroup6, "GameProjectGroup6" );
+ 
---
+++ Source/GameProjectGroup6/GameProjectGroup6GameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GameProjectGroup6GameMode.generated.h"
+
+UCLASS(minimalapi)
+class AGameProjectGroup6GameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AGameProjectGroup6GameMode();
+};
+
+
+
