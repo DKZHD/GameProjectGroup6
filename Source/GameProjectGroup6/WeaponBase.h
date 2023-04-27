63591f1582864fad855b47c8869984e6277b5ba5
DKZHD <dkvarberg@gmail.com>
2023-04-27 14:44:26 +0200

    Fixed Collision with Doorway again, Fixed Enemy Animation, fixed collision with enemy hand

--- Source/GameProjectGroup6/WeaponBase.h
+++ Source/GameProjectGroup6/WeaponBase.h
@@ -19,6 +19,8 @@ public:
 	UPROPERTY(EditAnywhere)
 		UStaticMeshComponent* Mesh;
 	class ABardPlayer* Bard;
+	UPROPERTY()
+	class AEnemy* Enemy;
 
 	UPROPERTY(EditAnywhere)
 	class UBoxComponent* FluteCollision;
